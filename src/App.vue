51dd518afa714d35bd0db603adec10849a1e2d58
Leon de Klerk <developer@leondeklerk.com>
2022-07-22 13:11:23 +0200

    Add implementation
    Signed-off-by: Leon de Klerk <developer@leondeklerk.com>

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,31 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
-  </div>
+    <div id="app">
+        <Qwixx />
+    </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
+import Qwixx from "./components/Qwixx.vue";
 
 @Component({
-  components: {
-    HelloWorld,
-  },
+    components: {
+        Qwixx
+    }
 })
 export default class App extends Vue {}
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 0.5em;
+    height: 100%;
+    margin-left: 0.5em;
+    margin-right: 0.5em;
 }
 </style>
