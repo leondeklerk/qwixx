93c714ca200a2846621da204746a55160c71edcc
Leon de Klerk <developer@leondeklerk.com>
2022-07-23 17:02:14 +0200

    Update application and docker
    Signed-off-by: Leon de Klerk <developer@leondeklerk.com>

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import App from "./App.vue";
-import "./registerServiceWorker";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 import "bootstrap/dist/css/bootstrap.css";
 
@@ -11,10 +10,10 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 /* import specific icons */
-import { faLockOpen, faLock, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { faLockOpen, faLock, faTimes, faSlash } from "@fortawesome/free-solid-svg-icons";
 
 /* add icons to the library */
-library.add(faLockOpen, faLock, faTimes);
+library.add(faLockOpen, faLock, faTimes, faSlash);
 
 /* add font awesome icon component */
 Vue.component("font-awesome-icon", FontAwesomeIcon);
